1865c0501bd1caa58c6f3c07f3e037a04eca3a5a
Devraj <devrajdora445@gmail.com>
2022-07-21 12:21:58 +0530

    'Successful'

--- eaglei/settings.py
+++ eaglei/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,6 +92,8 @@ DATABASES = {
        'PORT': '5432',
    }
 }
+db_from_env = dj_database_url.config(conn_max_age =600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
