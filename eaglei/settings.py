ca957c016349796d061c80867037e0c346b43647
Devraaz <devrajdora445@gmail.com>
2023-04-03 19:56:13 +0530

    Success

--- eaglei/settings.py
+++ eaglei/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,7 +83,7 @@ WSGI_APPLICATION = 'eaglei.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {}   
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
